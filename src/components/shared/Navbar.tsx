24cb57b3f8343b3bbe65788ef43e0830206cc9f3
s-kvng <kvngnathan8420@gmail.com>
2024-11-05 09:45:04 +0000

    some layout fixes & add aims logo

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { ThemeSwitcher } from "./theme-switcher";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className=" container flex justify-between items-center py-10 !px-32">
-      <div className="text-white">logo</div>
+    <div className=" container flex justify-between items-center py-5 md:!px-32">
+      <div className="text-white">
+        <Image src="/images/AIMS.png" width={60} height={60} alt="Logo image" />
+      </div>
       <div className="text-white flex justify-center items-center gap-x-3">
         <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" />
